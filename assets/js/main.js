62c7020fb0e26e89cf1c6a32158afca14c19b8eb
Cultura Difusa <47992536+Cultura-Difusa@users.noreply.github.com>
2024-01-06 21:25:18 -0300

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /*
-	Story by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Portales con Historia, web basada en | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license
 */
 
 	//TOPNAV HAMBURGER //
